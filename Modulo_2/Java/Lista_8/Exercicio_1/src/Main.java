792cb6b3f1b203e18386bd61c652b548296a2d02
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-05-28 17:31:37 -0300

    Feito
    Exercicio da lista 8 de POO feito.

--- Modulo_2/Java/Lista_8/Exercicio_1/src/Main.java
+++ Modulo_2/Java/Lista_8/Exercicio_1/src/Main.java
@@ -4,7 +4,11 @@ public class Main {
         grupo.adicionarHeroi(new Heroi(1, "Lyn", Arma.ESPADA));
         grupo.adicionarHeroi(new Heroi(2, "Marcus", Arma.LANCA));
         grupo.adicionarHeroi(new Heroi(3, "Dorcas", Arma.MACHADO));
-        grupo.adicionarHeroi(new Heroi(4, "Marcus", Arma.LANCA));
-        System.out.println(grupo.getHerois());
+
+        try{
+            grupo.adicionarHeroi(new Heroi(4, "Marcus", Arma.LANCA));
+        }catch (NomeRepetidoException exception){
+            System.out.println("Nome repetido! Favor, adicionar outro nome.");
+        }
     }
 }
