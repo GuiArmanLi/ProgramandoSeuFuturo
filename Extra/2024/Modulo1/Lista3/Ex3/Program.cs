f59c51c5be33bdb083a24eb5f827e3f47f5b8f12
Guilherme <guilhermesantanapessa12@gmail.com>
2024-03-03 23:30:10 -0300

    Adicionando lista 3

---
+++ Extra/2024/Modulo1/Lista3/Ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username = null;
+        string password = null;
+        int contador = 0;
+        do
+        {
+            Console.WriteLine("Digite nome de usuário:\t");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Digite a senha do usuário:\t");
+            password = Console.ReadLine();
+
+            if (username == "admin" && password == "123senha")
+            {
+                Console.WriteLine("Login realizado com sucesso");
+                break;
+            }
+            else if (contador == 2)
+            {
+                Console.WriteLine("Conta bloqueada!");
+                break;
+            }
+            contador++;
+        } while (username != "admin" && password != "123senha");
+    }
+}
